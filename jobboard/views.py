5ec725163aec164fd5d77fe5a1ab669499524ae0
gragra-kj <muthuigracia27@gmail.com>
2025-07-30 23:36:45 +0300

    register user

--- jobboard/views.py
+++ jobboard/views.py
@@ -4,6 +4,33 @@ from django.shortcuts import render
 from rest_framework import viewsets,permissions
 from .serializer import ApplicationSerializer,JobSerializer,CompanySerializer
 from .models import ApplicationModel,JobModel,CompanyProfile
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+
+#register
+
+@api_view(['POST'])
+def register_user(request):
+    username=request.data.get('username')
+    password=request.data.get('password')
+    
+    if username is None or password is None:
+        return Response({'error':'Please enter both password and username'},status=status.HTTP_400_BAD_REQUEST)
+    
+    if User.objects.filter(username=username).exists():
+        return Response({'error':'username already exists'},status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    user=User.objects.create_user(username=username,password=password)
+    token=Token.objects.create(user=user)
+    
+    return Response({'token':token.key},status=status.HTTP_201_CREATED)
+
+
 
 class ApplicationViewset(viewsets.ModelViewSet):
     #queryset=ApplicationModel.objects.all()
